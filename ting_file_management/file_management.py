129a85d4da400029bd7231ae6b296a4dbae20852
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-22 16:07:31 -0300

    feat: completando req-02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def is_txt_file(path_file: str) -> bool:
+    return path_file.split(".")[-1] == "txt"
+
+
+def txt_importer(path_file: str):
+    txt_stderr = sys.stderr
+    if not is_txt_file(path_file):
+        print("Formato inválido", file=txt_stderr)
+        return []
+    try:
+        with open(path_file) as file:
+            content = file.read()
+            return content.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=txt_stderr)
