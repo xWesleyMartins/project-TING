a4e8244d3516607bcdfa004cc64bfd3d48625f06
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-23 16:04:57 -0300

    feat: completando req-07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = [
+        {
+            "palavra": word,
+            "arquivo": queue["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, phrase in enumerate(queue["linhas_do_arquivo"])
+                if word.casefold() in phrase.casefold()
+            ],
+        }
+        for queue in instance.queue
+        if any(
+            word.casefold() in phrase.casefold()
+            for phrase in queue["linhas_do_arquivo"]
+        )
+    ]
+    return result
 
 
 def search_by_word(word, instance):
